764842036f2d375c30fcf29a8e11300e8e020f17
ArielJahn <ariel1.jahn@unidavi.edu.br>
2022-11-08 16:24:59 -0300

    aa

--- database/migrations/2022_11_07_175516_create_usuarios_table.php
+++ database/migrations/2022_11_07_175516_create_usuarios_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('usuarios', function (Blueprint $table) {
             $table->increments("id");
 
-            $table->string("login", 100)->unique;
+            $table->string("login", 100)->unique();
             $table->string("password", 255);
             $table->string("nome", 50);
 

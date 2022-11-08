d0d3b82735a3262c81677977209a981c70224c1c
ArielJahn <ariel1.jahn@unidavi.edu.br>
2022-11-08 13:21:04 -0300

    aa

--- database/migrations/2022_11_07_175524_create_enderecos_table.php
+++ database/migrations/2022_11_07_175524_create_enderecos_table.php
@@ -14,8 +14,23 @@ return new class extends Migration
     public function up()
     {
         Schema::create('enderecos', function (Blueprint $table) {
-            $table->id();
+            $table->increments();
+
+            $table->string("logradouro");
+            $table->string("numero");
+            $table->string("cidade");
+            $table->string("estado");
+            $table->string("cep");
+            $table->string("complemento");
+
+            $table->integer("usuario_id")
+                ->unsigned();
+
             $table->timestamps();
+
+            $table->foreign("usuario_id")
+            ->references("id")->on("usuarios")
+            ->onDelete("cascade");
         });
     }
 
